118342d82dba2d58d2487c155af62c1a7d264d81
EduCostam <eduardomc.devs@gmail.com>
2023-04-03 23:18:50 -0300

    App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
 import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Meu primeiro App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    height: 400,
+  },
+  title: {
+    fontSize: 24,
+    marginTop: 100,
+  },
+});
