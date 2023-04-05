1e26ec8cf0c7f16163f8a392cc1321ca746d1330
EduCostam <eduardomc.devs@gmail.com>
2023-04-05 17:09:38 -0300

    index.js home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,21 @@
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
+import { Logo } from "../../components/logo";
+
 export function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Página Home!!!</Text>
+      <Logo></Logo>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
+    flex: 1,
+    backgroundColor: "#F3F9FF",
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
   },
 });
