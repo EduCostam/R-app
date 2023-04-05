feb963652f62264d4e99fc6cbac036f2e4458f45
EduCostam <eduardomc.devs@gmail.com>
2023-04-05 17:14:47 -0300

    index.js styleLogoArea

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -2,8 +2,14 @@ import { View, Text, StyleSheet } from "react-native";
 
 export function Logo() {
   return (
-    <View>
+    <View style={styles.logoArea}>
       <Text>Receita Fácil</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logoArea: {
+    backgroundColor: "#4CBE6C",
+  },
+});
