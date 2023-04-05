7b4943e97823cd325385cb56ea3daf1710729fec
EduCostam <eduardomc.devs@gmail.com>
2023-04-05 17:25:50 -0300

    index.js Logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export function Logo() {
   return (
     <View style={styles.logoArea}>
-      <Text>Receita Fácil</Text>
+      <Text style={styles.logo}>Receita Fácil</Text>
     </View>
   );
 }
@@ -11,5 +11,19 @@ export function Logo() {
 const styles = StyleSheet.create({
   logoArea: {
     backgroundColor: "#4CBE6C",
+    alignSelf: "flex-start",
+    padding: 8,
+    paddingLeft: 16,
+    paddingRight: 20,
+    borderTopRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    borderTopLeftRadius: 8,
+    borderBottomRightRadius: 32,
+    marginBottom: 8,
+  },
+  logo: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FFF",
   },
 });
