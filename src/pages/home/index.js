6dc8b4b111390645f7066484ea0ef85b375d5281
EduCostam <eduardomc.devs@gmail.com>
2023-04-05 19:03:24 -0300

    index.js style || function search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   View,
   Text,
@@ -6,12 +7,16 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-
 import { Ionicons } from "@expo/vector-icons";
-
 import { Logo } from "../../components/logo";
 
 export function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSearch() {
+    console.log("VOCE CLICOU NESSE BOTAO");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Logo></Logo>
@@ -24,7 +29,7 @@ export function Home() {
           style={styles.input}
         ></TextInput>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleSearch}>
           <Ionicons name="search" size={28} color="#4CBE6C"></Ionicons>
         </TouchableOpacity>
       </View>
@@ -44,5 +49,24 @@ const styles = StyleSheet.create({
     fontSize: 26,
     fontWeight: "bold",
     color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#FFF",
+    whidth: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#ECECEC",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "90%",
+    maxWidth: "90%",
+    height: 54,
   },
 });
