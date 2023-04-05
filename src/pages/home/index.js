51a9cf8575309a92da63b6a02b4066628e2ef0bd
EduCostam <eduardomc.devs@gmail.com>
2023-04-05 18:19:24 -0300

    index.js home search styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,13 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
 
 import { Logo } from "../../components/logo";
 
@@ -6,9 +15,19 @@ export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Logo></Logo>
-
       <Text style={styles.title}>Encontre a receita</Text>
       <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+        ></TextInput>
+
+        <TouchableOpacity>
+          <Ionicons name="search" size={28} color="#4CBE6C"></Ionicons>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
