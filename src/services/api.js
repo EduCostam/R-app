b3ea773484dfa9a73b9487903bc663cb6d12a6f8
EduCostam <eduardomc.devs@gmail.com>
2023-04-06 12:18:58 -0300

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // Rodar com IPV4: json-server --watch -d 180 --host 192.168.186.1 db.json
 */
 
-const api = axios.create{
-  baseURL: 'http://192.168.186.1:3000/'
-}
+const api = axios.create({
+  baseURL: "http://192.168.186.1:3000/",
+});
 
 export default api;
